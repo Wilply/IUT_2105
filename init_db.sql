f9b488c0b633db1e5e5b0f19948f3cfb4a2c3e73
Wilply <clement.lecoq@protonmail.ch>
2018-05-22 21:44:09 +0200

    init_db.sql V1

--- init_db.sql
+++ init_db.sql
@@ -1,5 +1,29 @@
-DROP site_db;
+DROP DATABASE IF EXISTS site_db;
 
-CREATE site_db;
+CREATE DATABASE site_db;
 
 USE site_db;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+  user_id INT NOT NULL,
+  user_login VARCHAR(10) NOT NULL,
+  user_name VARCHAR(30) NOT NULL,
+  user_surname VARCHAR(40) NOT NULL,
+  user_password VARCHAR(40) NOT NULL,
+  user_mail VARCHAR(50) NOT NULL,
+  user_is_admin BOOLEAN NOT NULL);
+
+INSERT INTO users VALUES(0,"root","none","none","toto","root@email.fr",true);
+
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+  product_id INT NOT NULL,
+  product_name VARCHAR(50) NOT NULL,
+  producs_price DECIMAL(8,2) NOT NULL,
+  product_short_description TEXT,
+  product_description TEXT);
+
+COMMIT;
