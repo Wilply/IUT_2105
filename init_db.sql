576ec02a063a44643be320871ac9fad1d92c55c7
Wilply <clement.lecoq@protonmail.ch>
2018-05-24 16:01:00 +0200

    init_db.sql v2.1 and inscription v1.0

--- init_db.sql
+++ init_db.sql
@@ -10,37 +10,45 @@ USE db_clecoq001;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-  user_id INT NOT NULL,
+  user_id INT NOT NULL AUTO_INCREMENT,
   user_login VARCHAR(10) NOT NULL,
   user_name VARCHAR(30) NOT NULL,
   user_surname VARCHAR(40) NOT NULL,
   user_password VARCHAR(40) NOT NULL,
   user_mail VARCHAR(50) NOT NULL,
-  user_is_admin BOOLEAN NOT NULL);
+  user_is_admin BOOLEAN NOT NULL,
+  CONSTRAINT users_key PRIMARY KEY(user_id)
+);
 
 #ajout d un utilisateur administrateur pour la 1er connexion
-INSERT INTO users VALUES(0,"root","none","none","toto","root@email.fr",true);
+INSERT INTO users (user_login, user_name, user_surname, user_password, user_mail, user_is_admin)
+ VALUES ("root","none","none","toto","root@email.fr",true);
 
 DROP TABLE IF EXISTS products;
 
 CREATE TABLE products (
-  product_id INT NOT NULL,
+  product_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50) NOT NULL,
   producs_price DECIMAL(8,2) NOT NULL,
   product_short_description TEXT,
-  product_description TEXT);
+  product_description TEXT,
+  CONSTRAINT products_key PRIMARY KEY(product_id)
+);
 
 DROP TABLE IF EXISTS categories;
 
 CREATE TABLE categories (
-  cat_id INT NOT NULL,
-  cat_name VARCHAR(50) NOT NULL);
+  cat_id INT NOT NULL AUTO_INCREMENT,
+  cat_name VARCHAR(50) NOT NULL,
+  CONSTRAINT categories_key PRIMARY KEY(cat_id)
+);
 
 DROP TABLE IF EXISTS sub_categories;
 
 CREATE TABLE sub_categories (
-    subcat_id INT NOT NULL,
-    subcat_name VARCHAR(50) NOT NULL,
-    subcat_parent_id INT);
-
+  subcat_id INT NOT NULL AUTO_INCREMENT,
+  subcat_name VARCHAR(50) NOT NULL,
+  subcat_parent_id INT,
+  CONSTRAINT sub_categories_key PRIMARY KEY(subcat_id)
+);
 COMMIT;
--- inscription.php
+++ inscription.php
@@ -1,22 +1,48 @@
 <?php
-	
-	$db_connect = mysqli_connect("127.0.0.1", "root", "toto", "db_clecoq001");
-
-	function secure_input($string) {
-		$string = mysqli_real_escape_string($db_connect, $string);
+	function secure_input($string, $db) {
+		$string = mysqli_real_escape_string($db, $string);
         $string = addcslashes($string, '%_');
         return $string;
     }
 
-	$nom = secure_input($_POST['nom']);
-	$prenom = secure_input($_POST['prenom']);
-	$login = secure_input($_POST['login']);
-	$password = secure_input($_POST['password']);
-	$confirm_password = secure_input($_POST['confirm_password']);
-	$email = secure_input($_POST['email']);
-	$confirm_email = secure_input($_POST['confirm_email']);
+	function insert_db($login, $nom, $prenom, $password, $email, $db) {
+		$isAdmin = "false";
+		$value = "\"".$login."\",\"".$nom."\",\"".$prenom."\",\"".$password."\",\"".$email."\",".$isAdmin;
+		$requete = "INSERT INTO users (user_login, user_name, user_surname, user_password, user_mail, user_is_admin) VALUES (".$value.")";
+		#echo $requete;
+		$res = mysqli_query($db, $requete);
+		if ($res) {
+			echo '<Br>new record';
+		};
+	};
+
+	function get_user($db) {
+		$requete = "select * from users";
+		$res = mysqli_query($db, $requete);
+		for ($i = 1; $i <= mysqli_num_rows($res); $i++) {
+			echo "<Br>";
+			$data = mysqli_fetch_array($res);
+			for ($j = 0; $j < 7; $j++) {
+				echo $data[$j]."		";	
+			}
+		}
+	};
 
 	$isOk = true;
+
+	$db_connect = mysqli_connect("localhost", "toto", "totopwd", "db_clecoq001");
+	if (!$db_connect) {
+		#echo "Connot connect to mysql server";
+		$isOk =  false;
+	};
+
+	$nom = secure_input($_POST['nom'], $db_connect);
+	$prenom = secure_input($_POST['prenom'], $db_connect);
+	$login = secure_input($_POST['login'], $db_connect);
+	$password = secure_input($_POST['password'], $db_connect);
+	$confirm_password = secure_input($_POST['confirm_password'], $db_connect);
+	$email = secure_input($_POST['email'], $db_connect);
+	$confirm_email = secure_input($_POST['confirm_email'], $db_connect);
 
 	if($password != $confirm_password) {
 		$isOk = false;
@@ -25,8 +51,21 @@
 	};
 
 	if($isOk) {
-		echo "OK";
+		insert_db($login, $nom, $prenom, $password, $email, $db_connect);
+		#echo 'insert succesful';
+		header('Location: succes.html');
+		exit();
 	} else {
-		echo "NOT OK";
+		#echo 'insert failed';
+		header('Location: fail.html');
+		exit();
 	};
-?>
+
+	#TEST POUR VOIR COMMENT CA MARCHE CE BORDEL
+	#$test = "*";
+	#$requete = "SELECT ".$test." FROM users WHERE user_login=\"root\"";
+	#$res = mysqli_query($db_connect, $requete);
+	#echo mysqli_num_rows($res);
+	#$data = mysqli_fetch_array($res);
+	#echo $data[1];
+	#mysqli_free_result($res);
