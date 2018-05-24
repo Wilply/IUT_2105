c05c44362a9cf47420a6a0cf1777cef34ffe42e0
Wilply <clement.lecoq@protonmail.ch>
2018-05-24 19:57:29 +0200

    ahout d'un script pour traiter les erreurs d'inscription

--- inscription.php
+++ inscription.php
@@ -1,91 +1,51 @@
-<?php
-	function secure_input($string, $db) {
-		$string = mysqli_real_escape_string($db, $string);
-        $string = addcslashes($string, '%_');
-        return $string;
-    }
+<!DOCTYPE html>
+<html>
 
-	function insert_db($login, $nom, $prenom, $password, $email, $db) {
-		$result = true;
-		$password = password_hash($password, PASSWORD_DEFAULT);
-		$isAdmin = "false";
-		$value = "\"".$login."\",\"".$nom."\",\"".$prenom."\",\"".$password."\",\"".$email."\",".$isAdmin;
-		$requete = "INSERT INTO users (user_login, user_name, user_surname, user_password, user_mail, user_is_admin) VALUES (".$value.")";
-		#echo $requete;
-		$res = mysqli_query($db, $requete);
-	};
+	  <head>
+      <meta charset="utf-8">
+      <title> Inscription </title>
+      <link rel="stylesheet" type="text/css" href="stylesheet_inscription.css">
+	  </head>
+      <body class="backgroud">
+        <div class="login_div">
+          <div class="login_head">
+            Inscription
+          </div>
+          <div class="error_div">
+            <?php
+            if(isset($_GET['error_code'])) {
+              if(addcslashes($_GET['error_code'], '%_') == 1) {
+                echo "Vous avez entré 2 mot de passe differents";
+              } elseif(addcslashes($_GET['error_code'], '%_') == 2) {
+                echo "Vous avez entré 2 mot de passes differents";
+              } elseif(addcslashes($_GET['error_code'], '%_') == 3) {
+                echo "<Br>Ce pseudo est déja utilisée";
+              } elseif(addcslashes($_GET['error_code'], '%_') == 4) {
+                echo "Cette adresse email est déjà enregistrée";
+              }
+            }
+            ?>
+          </div>
+          <div class="login_input">
+            <form method="post" action="inscription_processing.php">
+                Nom:<br>
+                <input type="text" name="nom" class="login_input_field" placeholder="Nom d'utilisateur" autofocus required><br>
+                Prénom:<br>
+                <input type="text" name="prenom" class="login_input_field" placeholder="Nom d'utilisateur" autofocus required><br>
+                Nom d'utilisateur:<br>
+                <input type="text" name="login" class="login_input_field" placeholder="Nom d'utilisateur" autofocus required><br>
+                Mot de passe:<br>
+                <input type="Password" name="password" class="login_input_field" placeholder="Mot de passe" required ><br>
+                <input type="Password" name="confirm_password" class="login_input_field" placeholder="Retapez le Mot de passe" required ><br>
+                Adresse Email:<br>
+                <input type="email" name="email" class="login_input_field" size="30" placeholder="Email" required ><br>
+                <input type="email" name="confirm_email" class="login_input_field" size="30" placeholder="Confirmez l'adresse Email" required >
+            </div>
+          <div class="login_feet">
+            <input type="submit" name="sign up">
+              </form>
+          </div>
+        </div>
+	  </body>
 
-	function get_user($db) {
-		$requete = "select * from users";
-		$res = mysqli_query($db, $requete);
-		for ($i = 1; $i <= mysqli_num_rows($res); $i++) {
-			echo "<Br>";
-			$data = mysqli_fetch_array($res);
-			for ($j = 0; $j < 7; $j++) {
-				echo $data[$j]."		";	
-			}
-		}
-	};
-
-	function test_login_mail($login, $mail, $db) {
-		$isOk = true;
-		$req_login = "SELECT user_login FROM users WHERE user_login=\"".$login."\"";
-		$req_mail = "SELECT user_mail FROM users WHERE user_mail=\"".$mail."\"";
-		$row_login = mysqli_num_rows(mysqli_query($db, $req_login));
-		$row_mail = mysqli_num_rows(mysqli_query($db, $req_mail));
-		#echo "<Br>mail :".$row_mail;
-		#echo "<Br>login :".$row_login;
-		if (($row_login + $row_mail) > 0) {
-			$isOk = false;
-			#echo '<Br>il ya sup 0';
-		};
-		#echo $isOk ? 'true' : 'false';
-		return $isOk;
-	}
-
-	$isOk = true;
-
-	$db_connect = mysqli_connect("localhost", "toto", "totopwd", "db_clecoq001");
-	if (!$db_connect) {
-		#echo "Connot connect to mysql server";
-		$isOk =  false;
-	};
-
-	$nom = secure_input($_POST['nom'], $db_connect);
-	$prenom = secure_input($_POST['prenom'], $db_connect);
-	$login = secure_input($_POST['login'], $db_connect);
-	$password = secure_input($_POST['password'], $db_connect);
-	$confirm_password = secure_input($_POST['confirm_password'], $db_connect);
-	$email = secure_input($_POST['email'], $db_connect);
-	$confirm_email = secure_input($_POST['confirm_email'], $db_connect);
-
-	if($password != $confirm_password) {
-		$isOk = false;
-		#echo "<Br>2 pass diff";
-	} elseif ($email != $confirm_email) {
-		$isOk = false;
-		#echo "<Br>2 mail diff";
-	} elseif (!test_login_mail($login, $email, $db_connect)) {
-		$isOk = false;
-		#echo "<Br>mail or login already exist in db";
-	}
-
-	if($isOk) {
-		insert_db($login, $nom, $prenom, $password, $email, $db_connect);
-		#echo '<Br>insert succesful';
-		header('Location: succes.html');
-		exit();
-	} else {
-		#echo '<Br>insert failed';
-		header('Location: fail.html');
-		exit();
-	};
-
-	#TEST POUR VOIR COMMENT CA MARCHE CE BORDEL
-	#$test = "*";
-	#$requete = "SELECT ".$test." FROM users WHERE user_login=\"root\"";
-	#$res = mysqli_query($db_connect, $requete);
-	#echo mysqli_num_rows($res);
-	#$data = mysqli_fetch_array($res);
-	#echo $data[1];
-	#mysqli_free_result($res);
+	</html>
---
+++ inscription_processing.php
@@ -0,0 +1,115 @@
+<?php
+	function secure_input($string, $db) {
+		$string = mysqli_real_escape_string($db, $string);
+        $string = addcslashes($string, '%_');
+        return $string;
+    }
+
+	function insert_db($login, $nom, $prenom, $password, $email, $db) {
+		$result = true;
+		$password = password_hash($password, PASSWORD_DEFAULT);
+		$isAdmin = "false";
+		$value = "\"".$login."\",\"".$nom."\",\"".$prenom."\",\"".$password."\",\"".$email."\",".$isAdmin;
+		$requete = "INSERT INTO users (user_login, user_name, user_surname, user_password, user_mail, user_is_admin) VALUES (".$value.")";
+		#echo $requete;
+		$res = mysqli_query($db, $requete);
+	};
+
+	function get_user($db) {
+		$requete = "select * from users";
+		$res = mysqli_query($db, $requete);
+		for ($i = 1; $i <= mysqli_num_rows($res); $i++) {
+			echo "<Br>";
+			$data = mysqli_fetch_array($res);
+			for ($j = 0; $j < 7; $j++) {
+				echo $data[$j]."		";	
+			}
+		}
+	};
+
+	function test_login($login, $db) {
+		$isOk = true;
+		$req_login = "SELECT user_login FROM users WHERE user_login=\"".$login."\"";
+		$row_login = mysqli_num_rows(mysqli_query($db, $req_login));
+		#echo "<Br>login :".$row_login;
+		if ($row_login > 0) {
+			$isOk = false;
+			#echo '<Br>il ya sup 0';
+		};
+		#echo $isOk ? 'true' : 'false';
+		return $isOk;
+	}
+
+	function test_mail($mail, $db) {
+		$isOk = true;
+		$req_mail = "SELECT user_mail FROM users WHERE user_mail=\"".$mail."\"";
+		$row_mail = mysqli_num_rows(mysqli_query($db, $req_mail));
+		#echo "<Br>mail :".$row_mail;
+		if ($row_mail > 0) {
+			$isOk = false;
+			#echo '<Br>il ya sup 0';
+		};
+		#echo $isOk ? 'true' : 'false';
+		return $isOk;
+	}
+
+	$isOk = true;
+	$error_code = 0;
+	#ERROR CODE LIST:
+	#0 : NO ERROR
+	#1 : LES MDP SONT PAS IDENTIQUES
+	#2 : LES MAIL NE SONT PAS IDENTIQUE
+	#3 : PSEUDO DEJA UTILISE
+	#4 : EMAIL DEJA UTILISE
+
+	$db_connect = mysqli_connect("localhost", "toto", "totopwd", "db_clecoq001");
+	if (!$db_connect) {
+		#echo "Connot connect to mysql server";
+		$isOk =  false;
+	};
+
+	$nom = secure_input($_POST['nom'], $db_connect);
+	$prenom = secure_input($_POST['prenom'], $db_connect);
+	$login = secure_input($_POST['login'], $db_connect);
+	$password = secure_input($_POST['password'], $db_connect);
+	$confirm_password = secure_input($_POST['confirm_password'], $db_connect);
+	$email = secure_input($_POST['email'], $db_connect);
+	$confirm_email = secure_input($_POST['confirm_email'], $db_connect);
+
+	if($password != $confirm_password) {
+		$isOk = false;
+		$error_code = 1;
+		#echo "<Br>2 pass diff";
+	} elseif ($email != $confirm_email) {
+		$isOk = false;
+		$error_code = 2;
+		#echo "<Br>2 mail diff";
+	} elseif (!test_login($login, $db_connect)) {
+		$isOk = false;
+		$error_code = 3;
+		#echo "<Br>mail or login already exist in db";
+	} elseif (!test_mail($email, $db_connect)) {
+		$isOk = false;
+		$error_code = 4;
+		#echo "<Br>mail or login already exist in db";
+	}
+
+	if($isOk) {
+		insert_db($login, $nom, $prenom, $password, $email, $db_connect);
+		#echo '<Br>insert succesful';
+		header('Location: succes.html');
+		exit();
+	} else {
+		#echo '<Br>insert failed';
+		header('Location: inscription.php?error_code='.$error_code);
+		exit();
+	};
+
+	#TEST POUR VOIR COMMENT CA MARCHE CE BORDEL
+	#$test = "*";
+	#$requete = "SELECT ".$test." FROM users WHERE user_login=\"root\"";
+	#$res = mysqli_query($db_connect, $requete);
+	#echo mysqli_num_rows($res);
+	#$data = mysqli_fetch_array($res);
+	#echo $data[1];
+	#mysqli_free_result($res);
