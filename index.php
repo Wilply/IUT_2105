1a9c9f13a3ff77e09fd9bc066fc8a17993831b19
Wilply <clement.lecoq@protonmail.ch>
2018-05-26 22:35:45 +0200

    on a commencé la âge pricipale

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <!DOCTYPE >
 <html>
 <head>
-	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="stylesheet_index.css">
 	<title>PAGE PRICIPALE</title>
 </head>
-<body>
-	<div class="header">
-		<div class="logo"><img src="logoUPPA.png"></div>
-		<div class="search_bar">
+<body class="background">
+	<div class="main">
+		<div class="header">
+			<img class="logo" src="logoUPPA.png">
+			<form class="search_bar" action="search.php">
+				<input type="text" name="search" class="search_input">
+				<button type="submit" class="search_button"></button>
+			</form>
+				<form class="connexion" method="post" action="connexion.php"><input form="connexion_form" type="submit" value="connexion"></form>
+				<form id="inscription_button" class="connexion" method="post" action="inscription.php"><input form="inscription_form" type="submit" value="inscription"></form>
+		</div>
+		<div class="corp">
+			<div class="menu">
+				
+			</div>
+			<div class="article">
+				
+			</div>
+		</div>
+		<div class="footer">
 			
 		</div>
-		<div class="connexion">
-			
-		</div>
-	</div>
-	<div class="corp">
-		<div class="menu">
-			
-		</div>
-		<div class="article">
-			
-		</div>
-	</div>
-	<div class="footer">
-		
 	</div>
 </body>
 </html>
