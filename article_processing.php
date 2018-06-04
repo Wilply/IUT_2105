40c510471e7f7935f907d4c2ddde1319a17c1539
Clement Lecoq <clecoq001@rt-info-123.univ-pau.fr>
2018-06-04 18:45:41 +0200

    light modif & debug

--- article_processing.php
+++ article_processing.php
@@ -1,11 +1,21 @@
 <?php
 $db_connect = mysqli_connect("localhost", "u_clecoq001", "YwYL3tnj", "db_clecoq001");
+
+$id_produit = 1;
+$req_produit = "SELECT * FROM products WHERE product_id =".$id_produit;
+$produit = mysqli_fetch_array(mysqli_query($db_connect, $req_produit));
+
+$img = $produit[5];
 ?>
-<a>
-<div class="article_list">
+<div class="article">
   <div class="article_list_left">
     <div class="article_list_image">
-
+      <?php if ($img) {
+        echo $img;
+        echo "<img classe=\"image\" src=\"".$img."\" >";
+      } else {?>
+        <img class="image" src="no_img.png">
+      <?php }; ?>
     </div>
     <p class="article_list_price"></p>
   </div>
@@ -14,4 +24,3 @@ $db_connect = mysqli_connect("localhost", "u_clecoq001", "YwYL3tnj", "db_clecoq0
     <p class="article_list_short_description"></p>
   </div>
 </div>
-</a>
--- index.php
+++ index.php
@@ -9,40 +9,11 @@
 <body class="background">
 	<div class="main">
 		<?php include 'header.php'; ?>
-		<div class="corp">
-			<div class="menu">
-				<div id="first_prim" class="prim_cat">
-					<p class="prim_text">CATEGORIES</p>
-				</div>
-				<div class="prim_cat">
-					<p class="prim_text">CHAUSETTE</p>
-					<div class="sub_cat">
-						<a href="">ROUGE</a>
-						<a href="">RAYE</a>
-						<a href="">EN BOIS</a>
-					</div>
-				</div>
-				<div class="prim_cat">
-					<p class="prim_text">POISSON</p>
-					<div class="sub_cat">
-						<a href="">NEMO</a>
-						<a href="">DORIE</a>
-						<a href="">POSKAILLE</a>
-						<a href="">KRALAMOUR</a>
-					</div>
-				</div>
-				<div class="prim_cat">
-					<p class="prim_text">ANIMAUX</p>
-					<div class="sub_cat">
-						<a href="">IUT G1</a>
-						<a href="">IUT G2</a>
-						<a href="">GIGABYTE</a>
-					</div>
-				</div>
-			</div>
-			<div class="article">
-				<?php include 'article_processing.php'; ?>
-			</div>
+		<div class="corps">
+			<?php include 'menu.php'; ?>
+			<!--<div class="article">-->
+			<?php include 'article_processing.php'; ?>
+			<!--</div>-->
 		</div>
 		<div class="footer">
 
