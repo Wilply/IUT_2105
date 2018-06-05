2a6bfa3196a90358a670fa70225ccde5b607507d
Clement Lecoq <clecoq001@rt-tprzo2-202.univ-pau.fr>
2018-06-05 15:57:50 +0200

    update add_article.php

--- add_article.php
+++ add_article.php
@@ -26,12 +26,21 @@ $nom = $_POST['NomProd'];
 $prix = $_POST['PrixProd'];
 $short_descri = $_POST['ShortDescri'];
 $descri = $_POST['Descri'];
+
 $img_link = $_FILES['img_link']['name'];
 $img_type = $_FILES['img_link']['type'];
 $img_dir = $_FILES['img_link']['tmp_name'];
 $img_size = $_FILES['img_link']['size'];
+$img_error_code = $_FILES['img_link']['error'];
 
+$img_extension = substr($img_type,6,strlen($img_type)-6);
+$img_type = substr($_FILES['img_link']['type'],0,5);
 
+if ($img_type != "image") {
+  $isOk = false;
+};
+
+/*
 echo $nom."<Br>";
 echo $prix."<Br>";
 echo $short_descri."<Br>";
@@ -40,12 +49,26 @@ echo $img_link."<Br>";
 echo $img_type."<Br>";
 echo $img_dir."<Br>";
 echo $img_size."<Br>";
+echo $img_error_code."<Br>";
+echo $img_extension."<Br>";
+*/
 
-if ($img_dir) {
+#test si le dossier d upload existe ou le creer
+if (!file_exists("./upload/image") or is_file("./upload/image")) {
+
+};
+
+if ($img_error_code == 0 && $isOk) {
   if ($img_size > 102400) {
     header('Location: fail.html');
-  }
-} else {
+  };
+  $id = md5(uniqid(rand(), true));
+  $nom_local = "./upload/image/".$id.".".$img_extension;
+  $isUpload = move_uploaded_file($img_dir,$nom_local);
+  if ($isUpload) {
+    echo "c est upload";
+  };
+} elseif ($isOk) {
 
 };
 ?>
