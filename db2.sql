7b55d196025aa3c413146a8af87202e43000e4f3
Vincent Berhouet <vberhouet@rt-sta1-050.univ-pau.fr>
2018-06-12 10:37:24 +0200

    le fichier des produit oui oui oui

---
+++ db2.sql
@@ -0,0 +1,36 @@
+INSERT INTO categories (cat_name) VALUES ("ANIMAUX");
+
+INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ("Animaux d'intérieur", "Animaux d'extérieur");
+
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Escargot", "2","Petit escargot pour vos jardins","Petit escargot qui s'adapte à tous types de de jardins et de végétations. Il est hermaphrodite et donc n'a pas besoin d'un congénaire pour perpétuer son espèce.");
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Lézard", "7", "Lézard de taille moyenne (5cm)", "Lézard pour vivarium à alimenter avec des aliments BIO et que de la nourriture de mangeur de salade de vegan.");
+
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Dorade", "50", "Petite DORADE d'intérieur bleue", "La dorade est un animal d'intérieur qui s'adapte à tous types d'environnements. Elle vit en eau douce");
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Poisson Rouge", "70","Petit poisson d'intérieur","Ce genre de petit poisson rouge épicé m'amène, il broute de la pelouse comme des petites chèvres");
+
+
+
+
+INSERT INTO categories (cat_name) VALUES ("EMBALLAGES");
+
+INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ("Carton", "Plastiques");
+
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Carton Eco-responsable", "20", "Carton pour emballage d'objets de petite taille", "Ce carton est un carton qui respecte l'environnement car il est ondulé et composé à 100% de matériaux responsables et donc recyclables");
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Carton Basique", "10", "Carton basique pour emballage d'objets de taille moyenne", "Ce carton permet d'emballer des objets de taille moyenne. Il permet une sécurité lors d'un transport d'objets");
+
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Papier Bulle", "7", "Papier de protection", "Papier qui amortit les chocs lors des transports ou des livraisons d'objets de tout type.");
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Polystyrène", "5", "Emballage de polystyrène", "Emballage qui se constitue en petits morceaux de polystyrène qui permet une absorbtion des chocs dus à des transports.");
+
+
+
+
+
+INSERT INTO categories (cat_name) VALUES ("Ordinateurs");
+
+INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ("Fixes", "Portables");
+
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("PC Bureautique", "800", "Pc pour la bureautique", "Pc constitué de composants choisis spécifiquements pour la bureautique");
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("PC Gamer", "1200", "PC POUR LES JEUX", "PC constitué de composants spécifiques pour les gros gamers");
+
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("PC Bureautique", "800", "Pc pour la bureautique", "Pc constitué de composants choisis spécifiquements pour la bureautique");
+INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("PC Gamer", "1200", "PC POUR LES JEUX", "PC constitué de composants spécifiques pour les gros gamers");
