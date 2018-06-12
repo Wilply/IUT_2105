70aef483592f406c18bed0bfd3925841974b865a
Clement Lecoq <clecoq001@rt-sta1-049.univ-pau.fr>
2018-06-12 10:55:33 +0200

    bug fixes

--- db2.sql
+++ db2.sql
@@ -1,36 +1,40 @@
-INSERT INTO categories (cat_name) VALUES ("ANIMAUX");
+USE db_clecoq001;
 
-INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ("Animaux d'intérieur", "Animaux d'extérieur");
+INSERT INTO categories (cat_name) VALUES ('ANIMAUX');
 
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Escargot", "2","Petit escargot pour vos jardins","Petit escargot qui s'adapte à tous types de de jardins et de végétations. Il est hermaphrodite et donc n'a pas besoin d'un congénaire pour perpétuer son espèce.");
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Lézard", "7", "Lézard de taille moyenne (5cm)", "Lézard pour vivarium à alimenter avec des aliments BIO et que de la nourriture de mangeur de salade de vegan.");
+INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ('Animaux d\'interieur', 1);
 
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Dorade", "50", "Petite DORADE d'intérieur bleue", "La dorade est un animal d'intérieur qui s'adapte à tous types d'environnements. Elle vit en eau douce");
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Poisson Rouge", "70","Petit poisson d'intérieur","Ce genre de petit poisson rouge épicé m'amène, il broute de la pelouse comme des petites chèvres");
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('Escargot', '2','Petit escargot pour vos jardins','Petit escargot qui s adapte a tous types de de jardins et de vegetations. Il est hermaphrodite et donc n\'a pas besoin d\'un congenaire pour perpetuer son espece.', 1);
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('Lezard', '7', 'Lezard de taille moyenne (5cm)', 'Lezard pour vivarium a alimenter avec des aliments BIO et que de la nourriture de mangeur de salade de vegan.', 1);
 
-
-
-
-INSERT INTO categories (cat_name) VALUES ("EMBALLAGES");
-
-INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ("Carton", "Plastiques");
-
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Carton Eco-responsable", "20", "Carton pour emballage d'objets de petite taille", "Ce carton est un carton qui respecte l'environnement car il est ondulé et composé à 100% de matériaux responsables et donc recyclables");
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Carton Basique", "10", "Carton basique pour emballage d'objets de taille moyenne", "Ce carton permet d'emballer des objets de taille moyenne. Il permet une sécurité lors d'un transport d'objets");
-
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Papier Bulle", "7", "Papier de protection", "Papier qui amortit les chocs lors des transports ou des livraisons d'objets de tout type.");
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("Polystyrène", "5", "Emballage de polystyrène", "Emballage qui se constitue en petits morceaux de polystyrène qui permet une absorbtion des chocs dus à des transports.");
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('Dorade', '50', 'Petite DORADE d\'interieur bleue', 'La dorade est un animal d\'interieur qui s\'adapte a tous types d\'environnements. Elle vit en eau douce', 1);
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('Poisson Rouge', '70','Petit poisson d\'interieur','Ce genre de petit poisson rouge epice m\'amene, il broute de la pelouse comme des petites chevres', 1);
 
 
 
 
+INSERT INTO categories (cat_name) VALUES ('EMBALLAGES');
 
-INSERT INTO categories (cat_name) VALUES ("Ordinateurs");
+INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ('Carton', 2);
 
-INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ("Fixes", "Portables");
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('Carton Eco-responsable', '20', 'Carton pour emballage d\'objets de petite taille', 'Ce carton est un carton qui respecte l\'environnement car il est ondule et compose a 100% de materiaux responsables et donc recyclables', 2);
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('Carton Basique', '10', 'Carton basique pour emballage d\'objets de taille moyenne', 'Ce carton permet d\'emballer des objets de taille moyenne. Il permet une securite lors d\'un transport d\'objets', 2);
 
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("PC Bureautique", "800", "Pc pour la bureautique", "Pc constitué de composants choisis spécifiquements pour la bureautique");
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("PC Gamer", "1200", "PC POUR LES JEUX", "PC constitué de composants spécifiques pour les gros gamers");
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('Papier Bulle', '7', 'Papier de protection', 'Papier qui amortit les chocs lors des transports ou des livraisons d\'objets de tout type.', 2);
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('Polystyrene', '5', 'Emballage de polystyrene', 'Emballage qui se constitue en petits morceaux de polystyrene qui permet une absorbtion des chocs dus a des transports.', 2);
 
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("PC Bureautique", "800", "Pc pour la bureautique", "Pc constitué de composants choisis spécifiquements pour la bureautique");
-INSERT INTO products (product_name, producs_price, product_short_description, product_description) VALUES ("PC Gamer", "1200", "PC POUR LES JEUX", "PC constitué de composants spécifiques pour les gros gamers");
+
+
+
+
+INSERT INTO categories (cat_name) VALUES ('Ordinateurs');
+
+INSERT INTO sub_categories (subcat_name, subcat_parent_id) VALUES ('Fixes', 3);
+
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('PC Bureautique', '800', 'Pc pour la bureautique', 'Pc constitue de composants choisis specifiquements pour la bureautique', 3);
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('PC Gamer', '1200', 'PC POUR LES JEUX', 'PC constitue de composants specifiques pour les gros gamers', 3);
+
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('PC Bureautique', '800', 'Pc pour la bureautique', 'Pc constitue de composants choisis specifiquements pour la bureautique', 3);
+INSERT INTO products (product_name, product_price, product_short_description, product_description, product_sub_cat) VALUES ('PC Gamer', '1200', 'PC POUR LES JEUX', 'PC constitue de composants specifiques pour les gros gamers', 3);
+
+COMMIT;
